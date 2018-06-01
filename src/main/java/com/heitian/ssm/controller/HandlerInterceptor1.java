7347bf40e85a8d926f29c6ef959f74a4ad2a2936
yetaoshan <buaayts@163.com>
2018-06-01 10:04:05 +0800

    dev1 new

--- src/main/java/com/heitian/ssm/controller/HandlerInterceptor1.java
+++ src/main/java/com/heitian/ssm/controller/HandlerInterceptor1.java
@@ -32,6 +32,7 @@ public class HandlerInterceptor1 implements HandlerInterceptor {
 
         System.out.println("HandlerInterceptor1......preHandle");
         System.out.println();
+        System.out.println("dev-new");
         //return false表示拦截，不向下执行
         //return true表示放行
         return false;
